1ae0a6e397791be8a763db2cc4a7fd25db97274b
Carlos Pereira <carlospereira@ciandt.com>
2023-03-20 16:43:55 -0300

    Update: changed index page to only show my page title

--- projetos/ola-mundo/src/app/app.component.spec.ts
+++ projetos/ola-mundo/src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ola-mundo'`, () => {
+  it(`should have as title 'Olá mundo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Olá mundo');
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ola-mundo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Olá mundo app is running!');
   });
 });
