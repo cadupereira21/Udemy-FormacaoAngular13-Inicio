691a79960bc1145384a28d4a707274c2d7d5a00e
Carlos Pereira <carlospereira@ciandt.com>
2023-03-20 16:07:53 -0300

    Update: changed project name to Olá mundo

--- projetos/ola-mundo/src/app/app.component.spec.ts
+++ projetos/ola-mundo/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'ola-mundo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ola-mundo');
+    expect(app.title).toEqual('Olá mundo');
   });
 
   it('should render title', () => {
