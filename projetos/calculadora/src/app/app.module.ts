91a9041a3cd3bb230effa47fc92c548d4bfa451e
Carlos Pereira <carlospereira@ciandt.com>
2023-03-21 09:08:30 -0300

    Create: calculadora module and export class index

--- projetos/calculadora/src/app/app.module.ts
+++ projetos/calculadora/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//import { CalculadoraModule } from './calculadora/calculadora.module';
+import { CalculadoraModule } from './calculadora'; // arquivo calculadora/index.ts torna essa importação equivalente à importação acima! Apenas questões estéticas
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CalculadoraModule
   ],
   providers: [],
   bootstrap: [AppComponent]
