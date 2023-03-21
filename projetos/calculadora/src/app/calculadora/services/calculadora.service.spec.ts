d0683d42bff964a0e8fe427d8cfdec6aabd3cbc6
Carlos Pereira <carlospereira@ciandt.com>
2023-03-21 10:46:48 -0300

    Configured and created teste for calculadora service and component

--- projetos/calculadora/src/app/calculadora/services/calculadora.service.spec.ts
+++ projetos/calculadora/src/app/calculadora/services/calculadora.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora.service';
 
@@ -6,11 +6,43 @@ describe('CalculadoraService', () => {
   let service: CalculadoraService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [CalculadoraService]
+    });
     service = TestBed.inject(CalculadoraService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('deve garantir que 1+4 = 5',
+    inject([CalculadoraService], (service:CalculadoraService) => {
+      expect(service.calcular(1,4,CalculadoraService.SOMA)).toEqual(5);
+    })
+  );
+
+  it('deve garantir que 1-4 = -3',
+    inject([CalculadoraService], (service:CalculadoraService) => {
+      expect(service.calcular(1,4,CalculadoraService.SUBTRACAO)).toEqual(-3);
+    })
+  );
+
+  it('deve garantir que 1/4 = 0.25',
+    inject([CalculadoraService], (service:CalculadoraService) => {
+      expect(service.calcular(1,4,CalculadoraService.DIVISAO)).toEqual(0.25);
+    })
+  );
+
+  it('deve garantir que 1*4 = 4',
+    inject([CalculadoraService], (service:CalculadoraService) => {
+      expect(service.calcular(1,4,CalculadoraService.MULTIPLICACAO)).toEqual(4);
+    })
+  );
+
+  it('deve retornar 0 para operação inválida',
+    inject([CalculadoraService], (service:CalculadoraService) => {
+      expect(service.calcular(1,4,'%')).toEqual(0);
+    })
+  );
 });
