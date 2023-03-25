42cde175a264f6c737434ee133c8eba88603a0e5
Carlos Pereira <carlospereira@ciandt.com>
2023-03-25 10:45:21 -0300

    Create: implemented directives and attributes at listar-tarefas component html to show a list of tarefas

--- projetos/gerenciador-de-tarefas/src/app/tarefas/listar-tarefa/listar-tarefa.component.ts
+++ projetos/gerenciador-de-tarefas/src/app/tarefas/listar-tarefa/listar-tarefa.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { TarefaService, Tarefa } from '../shared';
 
 @Component({
   selector: 'app-listar-tarefa',
   templateUrl: './listar-tarefa.component.html',
   styleUrls: ['./listar-tarefa.component.css']
 })
-export class ListarTarefaComponent {
+export class ListarTarefaComponent implements OnInit {
+
+  tarefas: Tarefa[];
+
+  constructor(private tarefaService: TarefaService) {}
+
+  ngOnInit(): void {
+    //this.tarefas = this.listarTodos();
+    this.tarefas = [
+      new Tarefa(1, "Tarefa 1", false),
+      new Tarefa(2, "Tarefa 2", true),
+    ]
+  }
+
+  listarTodos(): Tarefa[]{
+    return this.tarefaService.listarTodos();
+  }
+
+
 
 }
