34538dc34a165d62824cf16a207bcac94a299fb9
Carlos Pereira <carlospereira@ciandt.com>
2023-03-25 11:08:03 -0300

    Create: implemented attributes and methods to cadastrar-tarefa component

--- projetos/gerenciador-de-tarefas/src/app/tarefas/cadastrar-tarefas/cadastrar-tarefas.component.ts
+++ projetos/gerenciador-de-tarefas/src/app/tarefas/cadastrar-tarefas/cadastrar-tarefas.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TarefaService, Tarefa } from '../shared';
 
 @Component({
   selector: 'app-cadastrar-tarefas',
   templateUrl: './cadastrar-tarefas.component.html',
   styleUrls: ['./cadastrar-tarefas.component.css']
 })
-export class CadastrarTarefasComponent {
+export class CadastrarTarefasComponent implements OnInit{
+
+  @ViewChild('formTarefa', { static: true }) formTarefa: NgForm;
+  tarefa: Tarefa;
+
+  constructor(private tarefaService: TarefaService, private router: Router){}
+
+  ngOnInit(): void {
+    this.tarefa = new Tarefa();
+  }
+
+  cadastrar(){
+    if(this.formTarefa.form.valid){
+      this.tarefaService.cadastrar(this.tarefa);
+      this.router.navigate(["/tarefas"])
+    }
+  }
 
 }
