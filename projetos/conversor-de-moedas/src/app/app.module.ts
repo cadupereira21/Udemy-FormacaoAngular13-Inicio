f9a89ddf17d50eda37535891a4516f8a4919cad1
Carlos Pereira <carlospereira@ciandt.com>
2023-03-27 10:29:27 -0300

    Create: created new conversor module files

--- projetos/conversor-de-moedas/src/app/app.module.ts
+++ projetos/conversor-de-moedas/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ConversorModule } from './conversor';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ConversorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
